22bf60a09a5ade6fb695573e634dc2375a97257e
kvp <kvipka90@yandex.ru>
2024-06-22 17:32:57 +0300

    implement selenide2

--- src/test/java/ru/example/projects/api_project/DemoApiTest.java
+++ src/test/java/ru/example/projects/api_project/DemoApiTest.java
@@ -2,6 +2,7 @@ package ru.example.projects.api_project;
 
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.example.projects.api_project.logic.DemoApiLogic;
 
@@ -11,6 +12,7 @@ public class DemoApiTest {
     @Test
     @DisplayName("1 : Тест первый")
     @Description("Описание")
+    @Tag("api")
     void simpleTest() {
         DemoApiLogic logic = new DemoApiLogic();
         logic.checkConfigFile();
--- src/test/java/ru/example/projects/playwright_dev/DemoPlaywrightTest.java
+++ src/test/java/ru/example/projects/playwright_dev/DemoPlaywrightTest.java
@@ -13,6 +13,7 @@ package ru.example.projects.playwright_dev;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.example.framework.playwright.BaseWebTest;
 import ru.example.projects.playwright_dev.logic.DemoPlaywrightLogic;
@@ -23,6 +24,7 @@ class DemoPlaywrightTest extends BaseWebTest {
     @Test
     @DisplayName("1 : Тест первый")
     @Description("Описание")
+    @Tag("playwright")
     void simpleTest(Page page) {
         DemoPlaywrightLogic logic = new DemoPlaywrightLogic(page);
         logic.openPage("https://playwright.dev/");
