2baed3012daa7953931471fdb17b1aafaebc39eb
kvp <kvipka90@yandex.ru>
2024-07-05 17:12:24 +0300

    remove dockertest19

--- src/test/java/ru/example/framework/playwright/PWOptions.java
+++ src/test/java/ru/example/framework/playwright/PWOptions.java
@@ -13,6 +13,7 @@ public class PWOptions implements OptionsFactory {
         // headless mode
         boolean headless = StringUtils.isEmpty(System.getProperty("headless")) || Boolean.parseBoolean(System.getProperty("headless"));
         options.setHeadless(headless);
+        options.setIgnoreHTTPSErrors(true);
         options.setContextOptions(new Browser.NewContextOptions()
                 .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
                 .setIgnoreHTTPSErrors(true)
