852218df70ff41dbdb908c36a9b32008e0bb11b8
justKvp <kvipka90@yandex.ru>
2024-10-15 10:25:03 +0300

    typo

--- src/test/java/ru/example/framework/listener/resultkeeper/SuiteResultData.java
+++ src/test/java/ru/example/framework/listener/resultkeeper/SuiteResultData.java
@@ -1,11 +1,9 @@
 package ru.example.framework.listener.resultkeeper;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 import java.util.ArrayList;
 
-@JsonIgnoreProperties({ "suiteName" })
 public class SuiteResultData {
     @Getter
     private final String suiteName;
